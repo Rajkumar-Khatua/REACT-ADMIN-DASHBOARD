bf48893d1171dd3b8aa0fea44a775892d139379a
Raj <rajkhatua2020@gmail.com>
2023-05-01 13:12:27 +0530

    Done New UserAndProduct page Day-3

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,75 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Login from './Pages/Login/Login';
-import List from './Pages/List/List';
-import Single from './Pages/Single/Single';
-import NewUserProduct from './Pages/New/NewUserProduct';
-import { userInputs } from './formDummtData';
+import Login from "./Pages/Login/Login";
+import List from "./Pages/List/List";
+import Single from "./Pages/Single/Single";
+import NewUserProduct from "./Pages/New/NewUserProduct";
+import { productInputs, userInputs } from "./formDummtData";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    },
-    {
-      path: "/login",
-      element: <Login/>,
-    },
-    {
-      path: "/users",
-      element: <List/>,
-    },
-    {
-      path: "/users/:id",
-      element: <Single/>,
-    },
-    {
-      path: "/new",
-      element: <NewUserProduct inputs={userInputs}/>,
-    },
-    {
-      path: "/products",
-      element: <List/>,
-    },
-    {
-      path: "/products/:productId",
-      element: <List/>,
-    },
-    
-  ]);
-  return <>
-  <RouterProvider router={router} />
-  </>;
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "/login",
+  //     element: <Login />,
+  //   },
+  //   {
+  //     path: "/users",
+  //     element: <List />,
+  //   },
+  //   {
+  //     path: "/users/:id",
+  //     element: <Single />,
+  //   },
+  //   {
+  //     path: "users/new",
+  //     element: <NewUserProduct inputs={userInputs} />,
+  //   },
+  //   {
+  //     path: "/products",
+  //     element: <List />,
+  //   },
+  //   {
+  //     path: "/products/:productId",
+  //     element: <List />,
+  //   },
+  // ]);
+  return (
+    <>
+      {/* <RouterProvider router={router} /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<NewUserProduct inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<NewUserProduct inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/Pages/New/NewUserProduct.jsx
+++ src/Pages/New/NewUserProduct.jsx
@@ -1,61 +1,61 @@
-import "./newUserProduct.scss"
-import Sidebar from '../../Components/Sidebar/Sidebar';
-import Navbar from '../../Components/Navbar/Navbar';
+import "./newUserProduct.scss";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Navbar from "../../Components/Navbar/Navbar";
 import { Camera, FileUpload } from "@mui/icons-material";
-const NewUserProduct = () => {
-  return (
-    <div className="newUserProduct">
-      <Sidebar/>
+import { useState } from "react";
+const NewUserProduct = ({ inputs, title }) => {
+  // create file upload state
+  const [file, setFile] = useState("")
 
-      <div className="newuserProductContainer">
-        <Navbar/>
-        <div className="top">
-          <h1 className="title">Add new User</h1>
+  return (
+    <div className='newUserProduct'>
+      <Sidebar />
+
+      <div className='newuserProductContainer'>
+        <Navbar />
+        <div className='top'>
+          <h1 className='title'>{title}</h1>
         </div>
-        <div className="bottom">
-          <div className="left">
-           <img src="https://icon-library.com/images/no-image-available-icon/no-image-available-icon-10.jpg" alt="" className="image" />
+        <div className='bottom'>
+          <div className='left'>
+            <img
+            // check first is there any inage is already exits ? if not just pass the second one 
+              src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-available-icon/no-image-available-icon-10.jpg"}
+              alt=''
+              className='image'
+            />
           </div>
-          <div className="right">
+          <div className='right'>
             <form>
-              <div className="formInput">
-                <label htmlFor="file">Image <FileUpload className="icon"/> </label>
-                <input type="file" id="file" style={{display:'none'}}/>
+              <div className='formInput'>
+                <label htmlFor='file'>
+                  Image <FileUpload className='icon' />
+                </label>
+                <input
+                  type='file'
+                  id='file'
+                  // set a file that will be uploaded
+                  onChange ={e=>setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </div>
-              <div className="formInput">
-                <label >Username</label>
-                <input type="text" placeholder="Kathalren"/>
+              {inputs.map((input)=>(
+                
+              <div className='formInput'>
+                <label>{input.label}</label>
+                <input
+                  type={input.type}
+                  placeholder={input.placeholder}
+                />
               </div>
-              <div className="formInput">
-                <label >Name and surname</label>
-                <input type="text" placeholder="landford"/>
-              </div>
-              <div className="formInput">
-                <label >Email</label>
-                <input type="email" placeholder="rajkhatua2020@gmail.com"/>
-              </div>
-              <div className="formInput">
-                <label >Phone </label>
-                <input type="text" placeholder="+1 8956 809 12"/>
-              </div>
-              <div className="formInput">
-                <label >Password </label>
-                <input type="password" />
-              </div>
-              <div className="formInput">
-                <label >Address </label>
-                <input type="password" placeholder="106 DOYERS ST. 8 ARLINGTON DR. 599 NW BAY BLVD."/>
-              </div>
-              <div className="formInput">
-                <label >Country </label>
-                <input type="password" placeholder="USA"/>
-              </div>
-              <button className="sendBtn">SEND</button>
+              ))}
+
+              <button className='sendBtn'>SEND</button>
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-export default NewUserProduct
+  );
+};
+export default NewUserProduct;
